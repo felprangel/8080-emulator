51f34c26dcee7f385c0c77d7860dc70abd4d7f21
felprangel <feliperangel897@gmail.com>
2026-02-17 11:37:14 -0300

    indentação

--- 8080.c
+++ 8080.c
@@ -5,7 +5,7 @@ typedef struct {
     uint8_t S:1;       // Sign
     uint8_t Z:1;       // Zero
     uint8_t P:1;       // Parity
-    uint8_t C:1;      // Carry
+    uint8_t C:1;       // Carry
     uint8_t AC:1;      // Auxiliary Carry
     uint8_t padding:3; // Padding (to complete 8 bits)
 } Flags;
@@ -13,7 +13,7 @@ typedef struct {
 typedef struct {
     uint16_t stack_pointer;
     uint16_t program_counter;
-    uint8_t *memory;
+    uint8_t *memory[0x10000];
 
     // Registers
     uint8_t A;
