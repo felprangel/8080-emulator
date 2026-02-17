e72ea17f04ed061fcb2ce75c163220730c33c007
felprangel <feliperangel897@gmail.com>
2026-02-17 11:16:48 -0300

    passa flags para um tipo separado

--- 8080.c
+++ 8080.c
@@ -2,6 +2,15 @@
 #include <stdint.h>
 
 typedef struct {
+    uint8_t S:1;       // Sign
+    uint8_t Z:1;       // Zero
+    uint8_t P:1;       // Parity
+    uint8_t C:1;      // Carry
+    uint8_t AC:1;      // Auxiliary Carry
+    uint8_t padding:3; // Padding (to complete 8 bits)
+} Flags;
+
+typedef struct {
     uint16_t *stack_pointer;
     uint16_t program_counter;
     uint8_t *memory;
@@ -12,16 +21,9 @@ typedef struct {
     uint8_t C;
     uint8_t D;
     uint8_t E;
+    Flags F;
     uint8_t H;
     uint8_t L;
-
-    // Flags
-    uint8_t S:1;       // Sign
-    uint8_t Z:1;       // Zero
-    uint8_t P:1;       // Parity
-    uint8_t CY:1;      // Carry
-    uint8_t AC:1;      // Auxiliary Carry
-    uint8_t padding:3; // Padding (to complete 8 bits)
 } cpu;
 
 
